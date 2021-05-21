7db558904f7600e98b92821e2ccd737542277ebc
austinogiza <austinogiza@gmail.com>
2021-05-21 22:29:00 +0100

    done styling repo

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,23 @@ const scrollWindow =()=>{
 
     }
 }
+
+const symbolStatus = document.querySelector(".symbol")
+const symbolStatusText = document.querySelector(".symbol h1")
+
+const hoverFn = ()=>{
+        symbolStatus.classList.add("active_status")
+        symbolStatusText.classList.add("status_h1")
+    }
+const hoverLeave = ()=>{
+        symbolStatus.classList.add("active_status")
+        symbolStatusText.classList.remove("status_h1")
+    }
+
+symbolStatus.addEventListener('mouseenter', hoverFn)
+symbolStatus.addEventListener('mouseleave', hoverLeave)
 window.addEventListener('scroll', scrollWindow)
+
+
+const fetch = require("node-fetch")
+
